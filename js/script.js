f3fa5512dea85885c66a882d6f3cba9ae2d8c658
Yosua Ardi <yosua4k@gmail.com>
2025-05-08 14:48:01 +0700

    Initial commit yosua portofolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// This is an empty JavaScript file.
+// You can add your JavaScript code here to make your website interactive.
+// For example, you can add code to:
+// - Handle form submissions
+// - Create animations
+// - Fetch data from an API
+// - Add dynamic content
+// - Respond to user interactions (clicks, hovers, etc.)
